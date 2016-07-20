4970d1af34c060541b4f8a931ded4b4f9f6ee0b6
we weia <we weia>
2016-07-21 00:02:21 +0800

    Signed-off-by: we weia <we weia>

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.apache.commons.io.IOUtils;
+
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		String uCanUup = "UCanUup";
+		String uri ="http://106.75.28.160/UCloud.txt#rd?n";
+		URL url;
+		InputStream is = null;
+		try {
+			url = new URL( uri);
+			URLConnection urlcon;
+			urlcon = url.openConnection();
+			urlcon.connect();
+			is = urlcon.getInputStream();
+			String text = "~~~" + IOUtils.toString(is, "UTF-8") + "~~~";
+			System.out.println("'UCanUup'出现的次数: "+ Integer.toString(text.split(uCanUup).length -1));
+		} catch (MalformedURLException e) {
+		} catch (IOException e) {
+		} finally {
+			try {
+				if (is != null)
+					is.close();
+			} catch (IOException e) {
+			}
+		}
+	}
+}
